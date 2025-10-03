dcee6409af0951bbbec7bbe5ab1619f253bb8a11
karavasi <olympia.karavasili_arapogianni@nokia.com>
2025-10-03 15:51:19 +0200

    Adding namespace argument in get_interfaces_status function

--- tests/common/devices/sonic.py
+++ tests/common/devices/sonic.py
@@ -1901,7 +1901,7 @@ Totals               6450                 6449
                     vlan_brief[vlan_name]["interface_ipv6"].append(prefix)
         return vlan_brief
 
-    def get_interfaces_status(self):
+    def get_interfaces_status(self, namespace=None):
         '''
         Get intnerfaces status by running 'show interfaces status' on the DUT, and parse the result into a dict.
 
@@ -1935,7 +1935,11 @@ Totals               6450                 6449
                 }
             }
         '''
-        return {x.get('interface'): x for x in self.show_and_parse('show interfaces status')}
+        if namespace is None:
+            return {x.get('interface'): x for x in self.show_and_parse('show interfaces status')}
+        else:
+            return {x.get('interface'): x for x in self.show_and_parse('show interfaces status -n {}'
+                                                                       .format(namespace))}
 
     def show_ipv6_interfaces(self):
         '''
