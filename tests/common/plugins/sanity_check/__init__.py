7440126ab4d91d0f8a7460559a4c4fef05ba2efa
Chenyang Wang <chenyangw233@gmail.com>
2025-12-16 22:25:05 +1100

    demo: disable parallel for print cmds

--- tests/common/plugins/sanity_check/__init__.py
+++ tests/common/plugins/sanity_check/__init__.py
@@ -110,9 +110,8 @@ def print_logs(duthosts, ptfhost, print_dual_tor_logs=False, check_ptf_mgmt=True
             outputs.append(res)
         logger.info("dut={}, cmd_outputs={}".format(dut.hostname, json.dumps(outputs, indent=4)))
 
-    with SafeThreadPoolExecutor(max_workers=8) as executor:
-        for duthost in duthosts:
-            executor.submit(print_cmds_output_from_duthost, duthost, print_dual_tor_logs, ptfhost)
+    for duthost in duthosts:
+        print_cmds_output_from_duthost(duthost, print_dual_tor_logs, ptfhost)
 
 
 def filter_check_items(tbinfo, duthosts, check_items):
